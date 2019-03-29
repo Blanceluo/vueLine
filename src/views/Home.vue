d4102f7d8fd552452b19f28ef9cd8e7829eeece8
Luo Jun <hasayaki97@gmail.com>
2019-03-29 11:29:25 +0800

    ss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,6 @@
   <div class="home">
     <h1>SKY WHEEL</h1>
     <div v-html="ctx"></div>
-    <iframe
-      style="display:none;"
-      frameborder="no"
-      border="0" marginwidth="0" marginheight="0"
-      width=330 height=86
-      src="//music.163.com/outchain/player?type=2&id=3932159&auto=1&height=66">
-    </iframe>
   </div>
 </template>
 
