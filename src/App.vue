fce52db02599cbb7c59493b330dae8843fa8ae78
Luo Jun <hasayaki97@gmail.com>
2019-03-29 12:42:55 +0800

    ss

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/about">About</router-link>
-      <router-link to="/">Home</router-link>
-    </nav>
     <router-view/>
     <!-- <iframe
       frameborder="no"
